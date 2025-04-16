c9b4819d35c596a4b157a7d9529527fa46b9649b
Roberto Orozco <roberto.o.r@outlook.com>
2025-04-16 10:36:08 -0600

    Add read/update functionality for document notifier

--- ghost_mark/lib/features/editor/application/document_notifier.dart
+++ ghost_mark/lib/features/editor/application/document_notifier.dart
@@ -1,15 +1,32 @@
+import 'dart:convert';
+
 import 'package:ghost_mark/features/editor/data/document.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'document_notifier.g.dart';
 
+const String documentPrefsKey = 'document';
+
 @riverpod
-class DocumentNotifier extends _$DocumentNotifier{
+class DocumentNotifier extends _$DocumentNotifier {
   @override
-  Document build() {
-    return Document(content: '');
+  Future<Document> build() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? documentJson = prefs.getString(documentPrefsKey);
+
+    if (documentJson == null) {
+      return Document(content: '');
+    }
+
+    return Document.fromJson(jsonDecode(documentJson));
   }
 
-  void updateDocument(String newText) {
+  Future<void> updateDocument(Document document) async {
+    final prefs = await SharedPreferences.getInstance();
+    final String documentJson = jsonEncode(document.toJson());
+    prefs.setString(documentPrefsKey, documentJson);
+
+    state = AsyncData(Document.fromJson(jsonDecode(documentJson)));
   }
 }
--- ghost_mark/lib/features/editor/application/document_notifier.g.dart
+++ ghost_mark/lib/features/editor/application/document_notifier.g.dart
@@ -6,12 +6,12 @@ part of 'document_notifier.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$documentNotifierHash() => r'eb709c7cb695605a81321a3c18a7f96026befb6f';
+String _$documentNotifierHash() => r'08f1a9ca502743e2179fc60ec8abb52190bb5408';
 
 /// See also [DocumentNotifier].
 @ProviderFor(DocumentNotifier)
 final documentNotifierProvider =
-    AutoDisposeNotifierProvider<DocumentNotifier, Document>.internal(
+    AutoDisposeAsyncNotifierProvider<DocumentNotifier, Document>.internal(
   DocumentNotifier.new,
   name: r'documentNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final documentNotifierProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$DocumentNotifier = AutoDisposeNotifier<Document>;
+typedef _$DocumentNotifier = AutoDisposeAsyncNotifier<Document>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
