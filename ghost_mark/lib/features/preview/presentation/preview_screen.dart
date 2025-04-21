1da060d9d35b8f6c40a36678c4787c9e291fe2ec
Roberto Orozco <roberto.o.r@outlook.com>
2025-04-21 10:56:28 -0600

    Refactor PreviewScreen to use HookConsumerWidget and manage document content state

--- ghost_mark/lib/features/preview/presentation/preview_screen.dart
+++ ghost_mark/lib/features/preview/presentation/preview_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
+import 'package:ghost_mark/features/editor/application/document_notifier.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class PreviewScreen extends StatelessWidget {
+class PreviewScreen extends HookConsumerWidget {
   const PreviewScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final document = ref.watch(documentNotifierProvider);
+    final content = useState(document.value?.content ?? '');
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
@@ -16,7 +22,7 @@ class PreviewScreen extends StatelessWidget {
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.black, width: 2.0),
               ),
-              child: Markdown(data: '# Hello World\nThis is a preview of your markdown content.'),
+              child: Markdown(data: content.value),
             ),
           ),
         ],
