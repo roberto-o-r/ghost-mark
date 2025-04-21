92b02b038e434163418272c9fa15345479264e18
Roberto Orozco <roberto.o.r@outlook.com>
2025-04-21 10:56:01 -0600

    Wrap MainApp in ProviderScope for Riverpod state management

--- ghost_mark/lib/main.dart
+++ ghost_mark/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ghost_mark/features/editor/presentation/main_screen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
