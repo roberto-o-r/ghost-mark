8c4643459f0a7e69f8304698cd3b485182da122a
Roberto Orozco <roberto.o.r@outlook.com>
2025-10-06 10:09:55 -0600

    Refactor CustomAppBar to add action buttons for open, save, and clear functionalities

--- ghost_mark/lib/shared/custom_app_bar.dart
+++ ghost_mark/lib/shared/custom_app_bar.dart
@@ -3,19 +3,43 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   final Size preferredSize;
+  final VoidCallback? onOpen;
+  final VoidCallback? onSave;
+  final VoidCallback? onClear;
 
-  const CustomAppBar({super.key})
-      : preferredSize = const Size.fromHeight(kToolbarHeight);
+  const CustomAppBar({super.key, this.onOpen, this.onSave, this.onClear})
+    : preferredSize = const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      centerTitle: true,
-      title: Image.asset(
-        'assets/images/text-logo.png',
-        height: 50,
+      centerTitle: false,
+      title: Row(
+        children: [
+          Image.asset(
+            'assets/images/text-logo.png',
+            height: 50,
+          ),
+        ],
       ),
       forceMaterialTransparency: true,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.folder_open),
+          tooltip: 'Open',
+          onPressed: onOpen,
+        ),
+        IconButton(
+          icon: const Icon(Icons.save),
+          tooltip: 'Save',
+          onPressed: onSave,
+        ),
+        IconButton(
+          icon: const Icon(Icons.clear),
+          tooltip: 'Clear',
+          onPressed: onClear,
+        ),
+      ],
     );
   }
 }
